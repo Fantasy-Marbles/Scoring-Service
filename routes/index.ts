64622c849c0d6996e1b7c200bc8f0162465c3f13
Luisa Scavo <luisascavo@gmail.com>
2022-09-30 20:29:36 -0400

    update routes

---
+++ routes/index.ts
@@ -0,0 +1,11 @@
+import express = require('express');
+import ExpressRouter =  express.Router; /* eslint-disable-line */
+
+const Router = new ExpressRouter();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
